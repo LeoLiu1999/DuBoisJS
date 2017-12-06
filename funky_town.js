ff181b6c99645fa5077e345acb1ee286d0bde33c
TakingTheL <yliu6@stuy.edu>
2017-12-06 09:16:09 -0500

    added extra console.log() statements for clarity

--- funky_town.js
+++ funky_town.js
@@ -4,11 +4,17 @@ var fibonacci = function(n){
     return fibonacci(n - 2) + fibonacci(n - 1)
 }
 
+console.log("Testing fibonacci...");
 console.log(fibonacci(0));
+console.log("should be 0");
 console.log(fibonacci(1));
+console.log("should be 1");
 console.log(fibonacci(2));
+console.log("should be 1");
 console.log(fibonacci(3));
+console.log("should be 2");
 console.log(fibonacci(4));
+console.log("should be 3");
 
 var gcd = function(a, b){
     if ( a % b  == 0)
@@ -16,8 +22,12 @@ var gcd = function(a, b){
     return gcd(b, a%b);
 }
 
+console.log(" ");
+console.log("Testing gcd...");
 console.log(gcd(1234,340));
-console.log(gcd(340,1234));
+console.log("should be 2");
+console.log(gcd(340,1234))
+console.log("should be 2");;
 
 var names = ["jasper", "leo", "eugene", "yuyang", "jen", "kelly", "helen", "queenie", "adam"];
 var randomStudent = function(){
@@ -27,6 +37,8 @@ var randomStudent = function(){
     return names[randIndex];
 }
 
+console.log(" ");
+console.log("Testing randomStudent...");
 console.log(randomStudent());
 console.log(randomStudent());
 console.log(randomStudent());
