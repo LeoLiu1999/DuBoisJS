041f70172d1cde95e65f7f035f7baec8b2f8f2ef
Leo <yliu6@stuy.edu>
2017-12-07 17:15:59 -0500

    done

--- funky_town.js
+++ funky_town.js
@@ -1,9 +1,50 @@
-var fibonacci = function(n){
+var fibonacciH = function(n){
     if (n < 2)
 	return n;
-    return fibonacci(n - 2) + fibonacci(n - 1)
+    return fibonacciH(n - 2) + fibonacciH(n - 1)
 }
 
+var fibonacci = function(){
+    console.log(fibonacciH(6));
+}
+
+var gcdH = function(a, b){
+    if ( a % b  == 0)
+	return b;
+    return gcdH(b, a%b);
+}
+
+var gcd = function(){
+    console.log(gcdH(36, 40));
+}
+
+var names = ["jasper", "leo", "eugene", "yuyang", "jen", "kelly", "helen", "queenie", "adam"];
+var randomStudent = function(){
+    var len = names.length;
+    var randIndex = Math.floor((len) * Math.random());
+    console.log(names[randIndex]);
+    return names[randIndex];
+}
+
+var buttonCallback = function(e){
+    console.log("\n\nhere comes e...");
+    console.log(e);
+    console.log("\n\nhere comes 'this'...");
+    console.log(this);
+}
+var b = document.getElementById('b');
+b.addEventListener("click", buttonCallback);
+
+var fib = document.getElementById('fib');
+fib.addEventListener("click", fibonacci);
+
+var gcdA = document.getElementById('gcd'); //gcdA because naming conflicts...
+gcdA.addEventListener("click", gcd);       //Will otherwise overwrite gcd() the function
+
+var randStu = document.getElementById('randStu');
+randStu.addEventListener("click", randomStudent);
+
+/*
 console.log("Testing fibonacci...");
 console.log(fibonacci(0));
 console.log("should be 0");
@@ -14,50 +55,16 @@ console.log("should be 1");
 console.log(fibonacci(3));
 console.log("should be 2");
 console.log(fibonacci(4));
-console.log("should be 3");
+console.log("should be 3");console.log(" ");
 
-var gcd = function(a, b){
-    if ( a % b  == 0)
-	return b;
-    return gcd(b, a%b);
-}
-
-console.log(" ");
 console.log("Testing gcd...");
 console.log(gcd(1234,340));
 console.log("should be 2");
 console.log(gcd(340,1234))
-console.log("should be 2");;
+console.log("should be 2");
 
-var names = ["jasper", "leo", "eugene", "yuyang", "jen", "kelly", "helen", "queenie", "adam"];
-var randomStudent = function(){
-    var len = names.length;
-    var randIndex = Math.floor((len) * Math.random());
-    //console.log(randIndex);
-    return names[randIndex];
-}
-
-console.log(" ");
 console.log("Testing randomStudent...");
 console.log(randomStudent());
 console.log(randomStudent());
 console.log(randomStudent());
-
-
-var buttonCallback = function(e){
-    console.log("\n\nhere cones e...");
-    console.log(e(5));
-    console.log("\n\nhere comes 'this'...");
-    console.log(this);
-}
-var b = document.getElementById('b');
-b.addEventListener("click", buttonCallback);
-
-var fib = document.getElementById('fib');
-fib.addEventListener("click", fibonacci(5));
-
-var gcd = document.getElementById('gcd');
-gcd.addEventListener("click", buttonCallback);
-
-var randStu = document.getElementById('randStu');
-randStu.addEventListener("click", buttonCallback);
+*/
